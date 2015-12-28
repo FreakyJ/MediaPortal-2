183c724eb9258b5409e1899922a11353a82b0473
FreakyJ <JensMarkwart@gmx.de>
2015-12-28 08:16:10 +0100

    random test changes

---
+++ MediaPortal/Incubator/AspNetServer/HandleModelbinding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.SwaggerGen.Generator;
+
+namespace MediaPortal.Plugins.AspNetServer
+{
+    public class HandleModelbinding : IOperationFilter
+  {
+    public void Apply(Operation operation, OperationFilterContext context)
+    {
+      if (operation.Parameters == null) return;
+
+      foreach (IParameter param in operation.Parameters)
+      {
+        if (param.In == "modelbinding")
+          param.In = "query";
+      }
+    }
+  }
+}
--- MediaPortal/Incubator/AspNetServer/Startup.cs
+++ MediaPortal/Incubator/AspNetServer/Startup.cs
@@ -23,43 +23,75 @@
 #endregion
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
 using MediaPortal.Plugins.AspNetServer.Logger;
 using MediaPortal.Plugins.AspNetServer.PlatformServices;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.FileProviders;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.ApiExplorer;
+using Microsoft.AspNet.Routing;
 using Microsoft.AspNet.StaticFiles;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.SwaggerGen.Generator;
+using Swashbuckle.SwaggerGen.Application;
 
 namespace MediaPortal.Plugins.AspNetServer
 {
+  /// <summary>
+  /// was that missing?!
+  /// </summary>
   public class Item
   {
+    /// <summary>
+    /// or that
+    /// </summary>
     public int Id;
+    /// <summary>
+    /// or this
+    /// </summary>
     public string Name;
   }
 
+  /// <summary>
+  /// Some summary
+  /// </summary>
   [Route("api/[Controller]")]
+  [Produces("application/json", "text/json")]
   public class ItemsController : Controller
   {
+    /// <summary>
+    /// Return all items
+    /// </summary>
     public static List<Item> Items = new List<Item>
     {
       new Item { Id = 1, Name = "First Test Item" },
       new Item { Id = 2, Name = "Second Test Item" },
     };
 
+    /// <summary>
+    /// Get all items
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     public IEnumerable<Item> Get()
     {
       return Items;
     }
 
-    [HttpGet("{id}")]
-    public Item Get(int id)
+    /// <summary>
+    /// Get Item by Id
+    /// </summary>
+    /// <param name="id">A var</param>
+    /// <returns></returns>
+    [HttpGet("itemId")]
+    public Item Get([FromQuery]int id, int dummy)
     {
       return Items.FirstOrDefault(item => item.Id == id);
     }
@@ -67,22 +99,47 @@ namespace MediaPortal.Plugins.AspNetServer
 
   public class Startup
   {
+    private static readonly Assembly ASS = Assembly.GetExecutingAssembly();
+    private static readonly string ASSEMBLY_PATH = Path.GetDirectoryName(ASS.Location);
+
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddSingleton<IApplicationEnvironment>(new MP2ApplicationEnvironment());
       services.AddTransient(typeof(ILibraryManager), typeof(MP2LibraryManager));
       services.AddMvc();
+      services.AddSwaggerGen(c =>
+      {
+        c.DescribeAllEnumsAsStrings();
+        c.OperationFilter<HandleModelbinding>();
+        //c.IncludeXmlComments(Path.Combine(ASSEMBLY_PATH, ASS.GetName().Name+".xml"));
+        c.SingleApiVersion(new Info
+        {
+          Title = "MP2Extended API",
+          Description = "MP2Extended brings the well known MPExtended from MP1 to MP2",
+          Contact = new Contact
+          {
+            Name = "FreakyJ"
+          },
+          Version = "v1"
+        });
+      });
     }
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseSwaggerUi(swaggerUrl: "/swagger/v1/swagger.json");
+
+      string resourcePath = Path.Combine(ASSEMBLY_PATH, "www").TrimEnd(Path.DirectorySeparatorChar);
       app.UseFileServer(new FileServerOptions
       {
-        FileProvider = new PhysicalFileProvider(@"C:\"),
-        RequestPath = new PathString("/StaticFiles"),
+        FileProvider = new PhysicalFileProvider(resourcePath),
+        RequestPath = new PathString("/swagger/ui"),
         EnableDirectoryBrowsing = true,
       });
+      // Configure the HTTP request pipeline.
+      app.UseStaticFiles();
       app.UseMvc();
+      app.UseSwaggerGen();
       app.Run(context => context.Response.WriteAsync("Hello World"));
     }
   }
